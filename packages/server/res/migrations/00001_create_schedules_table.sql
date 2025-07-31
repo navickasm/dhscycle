d110428f678892c635c82429fa1a0de1c6f2e232
Mack Navickas <mjnavickas@gmail.com>
2025-07-31 16:27:56 -0500

    Added no school thing

--- packages/server/res/migrations/00001_create_schedules_table.sql
+++ packages/server/res/migrations/00001_create_schedules_table.sql
@@ -1,12 +1,14 @@
 CREATE TABLE schedules (
     date DATE PRIMARY KEY,
-    regularity TEXT NOT NULL CHECK (regularity IN ('A', '16', '27', '38', '45', 'special')),
-    special_schedule_name TEXT,
+    regularity TEXT NOT NULL CHECK (regularity IN ('A', '16', '27', '38', '45', 'special', 'no')),
+    special_schedule_name TEXT, -- This is also where no school reasons go
     special_schedule_h2 TEXT,
     schedule_json TEXT,
     CHECK (
         (regularity = 'special' AND special_schedule_name IS NOT NULL AND special_schedule_h2 IS NOT NULL AND schedule_json IS NOT NULL)
             OR
-        (regularity <> 'special' AND special_schedule_name IS NULL AND special_schedule_h2 IS NULL AND schedule_json IS NULL)
+        (regularity = 'no' AND special_schedule_name IS NOT NULL AND special_schedule_h2 IS NULL AND schedule_json IS NULL)
+            OR
+        (regularity <> 'special' AND regularity <> 'no' AND special_schedule_name IS NULL AND special_schedule_h2 IS NULL AND schedule_json IS NULL)
         )
 );
