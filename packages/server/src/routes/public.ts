f990c1f400545bfbc860a8a6f33e000b35167ee0
Mack Navickas <mjnavickas@gmail.com>
2025-08-15 14:05:54 -0500

    fixed problem

--- packages/server/src/routes/public.ts
+++ packages/server/src/routes/public.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import { fileURLToPath } from "url";
 import path from "path";
 
 const router = express.Router();
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 router.use(express.static(path.join(__dirname, "../../res/public")));
 
 export default router;
