2d0235f6e7267eb2f2c8dbf6be43c2e04b9807ce
Mack Navickas <mjnavickas@gmail.com>
2025-08-22 11:51:33 -0500

    Added invalidateCache route

--- packages/server/src/routes/admin.ts
+++ packages/server/src/routes/admin.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import dotenv from 'dotenv';
 import {populateDb} from '../service.js';
+import {invalidateCaches} from "../cache.js";
 
 const router = Router();
 
@@ -18,6 +19,15 @@ router.post('/admin/populate', async (req, res) => {
         console.error('Error populating database:', error);
         res.status(500).json({ message: 'Internal Server Error' });
     });
+});
+
+router.post('admin/invalidateCache', (req, res) => {
+    const apiKey = req.headers['x-api-key'];
+    if (apiKey !== process.env.ADMIN_API_KEY) {
+        return res.status(403).json({ message: 'Forbidden: Invalid API Key' });
+    }
+    invalidateCaches();
+    res.status(200).json({ message: 'Caches invalidated successfully.' });
 });
 
 export default router;
