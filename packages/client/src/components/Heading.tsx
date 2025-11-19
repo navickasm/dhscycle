971b22d2c914d521191d6f26372b09856afd5ca1
Mack Navickas <mjnavickas@gmail.com>
2025-11-19 09:13:16 -0600

    Centered h2

--- packages/client/src/components/Heading.tsx
+++ packages/client/src/components/Heading.tsx
@@ -26,7 +26,7 @@ export default function Heading(p: HeadingProps) {
         <>
             <h1 style={{ textAlign: "center" }}>{date}</h1>
 
-            {p.h2 && <h2> {p.h2.split('%%').map((part, i) => (
+            {p.h2 && <h2 style={{ textAlign: "center" }}> {p.h2.split('%%').map((part, i) => (
                 <React.Fragment key={i}>
                     {i > 0 && <br />}
                     {part}
