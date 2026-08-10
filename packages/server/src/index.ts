ca41d16d221ba0798101559be2682be25f96a629
Mack Navickas <mjnavickas@gmail.com>
2026-08-10 17:55:18 -0500

    added catch-all 404 and error handler for CORS

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import Express from 'express';
+import Express, {NextFunction, Request, Response} from 'express';
 import dotenv from 'dotenv';
 
 import routes from './routes/index.js';
@@ -17,6 +17,22 @@ const app = Express();
 
 app.use(corsMiddleware);
 app.use(routes);
+
+app.use((req: Request, res: Response) => {
+    res.status(404).json({ message: 'Not Found' });
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err.message === 'Not allowed by CORS') {
+        return res.status(403).json({ message: 'Forbidden: Origin not allowed' });
+    }
+
+    console.error('Unhandled error:', err);
+    if (res.headersSent) {
+        return next(err);
+    }
+    return res.status(500).json({ message: 'Internal Server Error' });
+});
 
 app.listen(4000, () => {
     console.log("API Server online");
